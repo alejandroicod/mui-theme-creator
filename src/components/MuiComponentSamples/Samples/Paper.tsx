97e41df1047488a54091686ed2561c3e38964b1e
Alejandro Henriquez Luis <contacto@alejandrohenriquez.es>
2023-06-22 10:34:38 +0100

    Hosting update

--- src/components/MuiComponentSamples/Samples/Paper.tsx
+++ src/components/MuiComponentSamples/Samples/Paper.tsx
@@ -5,7 +5,7 @@ import Paper from '@mui/material/Paper';
 export default function SimplePaper() {
   return (
     <div>
-      <Typography variant="h6">Paper Elevations (0-24)</Typography>
+      <Typography variant="h6">Paper Elevations (4-10)</Typography>
       <Box
         sx={{
           mb: 2,
@@ -18,13 +18,10 @@ export default function SimplePaper() {
           },
         }}
       >
-        <Paper elevation={0} />
-        <Paper elevation={4} />
-        <Paper elevation={8} />
-        <Paper elevation={12} />
-        <Paper elevation={16} />
-        <Paper elevation={20} />
-        <Paper elevation={24} />
+        <Paper elevation={4} sx={{ p: 2 }}><Typography variant="h6">4</Typography></Paper>
+        <Paper elevation={6} sx={{ p: 2 }}><Typography variant="h6">6</Typography></Paper>
+        <Paper elevation={8} sx={{ p: 2 }}><Typography variant="h6">8</Typography></Paper>
+        <Paper elevation={10} sx={{ p: 2 }}><Typography variant="h6">10</Typography></Paper>
       </Box>
       <Typography variant="h6">Outlined Paper</Typography>
       <Box
